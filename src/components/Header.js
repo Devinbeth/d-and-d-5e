03fd6e4099f5fd983a8a5945996c64feb36a7bea
Devin Lethbridge <devinbeth@gmail.com>
2018-02-10 02:36:01 -0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header(){
     return (
         <div className="header">
             <button>Home</button>
+            <button>Donate</button>
             <button>About</button>
             <button>Contact</button>
         </div>
